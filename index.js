ee36057555b564088d0180420d9cec4476d8d9ca
xbayazid <bayazid9006@gmail.com>
2022-11-05 19:33:59 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
+const port = process.env.PORT || 5000;
+const {MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b699yx9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res)=>{
+    res.send('Genius car server is running');
+})
+
+app.listen(port, ()=>{
+    console.log(`genius car server running on port ${port}`);
+})
