005a8bd0faae8f6b7d50fa85ae223afbc6543b2e
xbayazid <bayazid9006@gmail.com>
2022-11-07 18:43:35 +0600

    done

--- index.js
+++ index.js
@@ -1,63 +1,58 @@
+const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+require('dotenv').config();
 
-// middle wares
+//middlewares
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b699yx9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-    try {
+async function run(){
+    try{
         const serviceCollection = client.db('geniusCar').collection('services');
         const orderCollection = client.db('geniusCar').collection('orders');
 
-        app.get('/services', async (req, res) => {
+        app.get('/services', async(req, res)=>{
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
-
-        app.get('/services/:id', async (req, res) => {
+        app.get('/services/:id', async(req, res)=>{
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = {_id: ObjectId(id)}
             const service = await serviceCollection.findOne(query);
-            res.send(service);
+            res.send(service)
         });
 
-
         // orders api
-        app.get('/orders', async (req, res) => {
-            let query = {};
-
-            if (req.query.email) {
-                query = {
+        app.get('/orders', async(req, res)=>{
+            let query = {}
+            if(req.query.email){
+                query={
                     email: req.query.email
                 }
             }
-
             const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+            const order = await cursor.toArray();
+            res.send(order);
         });
 
-        app.post('/orders', async (req, res) => {
+        app.post('/orders', async(req, res)=>{
             const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
+            const result = orderCollection.insertOne(order);
+            res.send(result)
         });
 
-        app.patch('/orders/:id', async (req, res) => {
+        app.patch('orders/:id', async(req, res)=>{
             const id = req.params.id;
-            const status = req.body.status
-            const query = { _id: ObjectId(id) }
+            const status = req.body.status;
+            const query = {_id: ObjectId(id)}
             const updatedDoc = {
                 $set:{
                     status: status
@@ -66,29 +61,26 @@ async function run() {
             const result = await orderCollection.updateOne(query, updatedDoc);
             res.send(result);
         })
-
-        app.delete('/orders/:id', async (req, res) => {
+    
+        app.delete('/orders/:id', async(req, res)=>{
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = {_id: ObjectId(id)};
             const result = await orderCollection.deleteOne(query);
             res.send(result);
-        })
-
+        });
+    }
+    finally{
 
     }
-    finally {
-
-    }
-
 }
 
-run().catch(err => console.error(err));
+run().catch(error => console.error(error));
 
 
-app.get('/', (req, res) => {
-    res.send('genius car server is running')
+app.get('/', (req, res)=>{
+    res.send('Genius car server is running');
 })
 
-app.listen(port, () => {
-    console.log(`Genius Car server running on ${port}`);
+app.listen(port, ()=>{
+    console.log(`genius car server running on port ${port}`);
 })
